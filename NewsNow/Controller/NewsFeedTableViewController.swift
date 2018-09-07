ff92910ee296557824329eead19527d11aa5c0b8
Anand Nigam <anandnigam.2aug@gmail.com>
2018-09-07 15:19:53 +0530

    Essential Pods incorporated.

---
+++ NewsNow/Controller/NewsFeedTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NewsFeedTableViewController.swift
+//  NewsNow
+//
+//  Created by Anand Nigam on 07/09/18.
+//  Copyright © 2018 Anand Nigam. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+ 
+}
